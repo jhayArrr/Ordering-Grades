b3c89e1b23e82cdd1432181601767ab6b6f514f0
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-18 11:17:26 -0600

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const marks = [56, 75, 84, 95, 88, 79]; //add grades as an array
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
 
 //This function uses another function, call Single Concern Function (able to use either function for various purposes)
-function calculatedGrade (marks) {
+function calculatedGrade (average) {
   const average = calculateAverage (marks);
   if (average < 60) return  'F';
   if (average < 69) return  'D';
