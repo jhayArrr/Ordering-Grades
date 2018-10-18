e535b987c941def46a14b7686b2ef557a0fe5df5
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-18 11:19:23 -0600

    changess

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const marks = [56, 75, 84, 95, 88, 79]; //add grades as an array
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
 
 //This function uses another function, call Single Concern Function (able to use either function for various purposes)
-function calculatedGrade (average) {
-  const average = calculateAverage (marks);
+function calculatedGrade(average) {
+  const average = calculateAverage(marks);
   if (average < 60) return  'F';
   if (average < 69) return  'D';
   if (average < 79) return  'C';
@@ -16,6 +16,6 @@ function calculteAverage (array) {
   let sum = 0;
   for (let mark of marks) sum += mark; //Example of reading an array and one line to add up the sum of all the grades
   return sum / marks.length; //"Divide the total marks by the number of grades"
-}
 
-console.log ("My letter grade is", calculatedGrade (average), ";because my numerical average is", calculateAverage (marks));
+console.log ("My letter grade is", calculatedGrade(average), ";because my numerical average is", calculateAverage (marks));
+}
