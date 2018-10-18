dc4bb7551e35111c0d0023674c6e07a52241acf3
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-18 11:31:52 -0600

    changes

--- index.js
+++ index.js
@@ -16,4 +16,5 @@ function calculatedGrade (average) {
     let sum = 0;
     for (let mark of marks) sum += mark; //Example of reading an array and one line to add up the sum of all the grades
     return sum / marks.length; //"Divide the total marks by the number of grades"
+
   }
