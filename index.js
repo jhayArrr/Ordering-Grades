58c35453c1d9b37d86d1a7a59874c330c8853ddd
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-18 11:23:08 -0600

    changes

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ function calculatedGrade (average) {
   if (average < 79) return  'C';
   if (average < 89) return  'B';
   return A; //This is the last grade left, using the funnel image (metaphor)
-}
-function calculteAverage (array) {
-  let sum = 0;
-  for (let mark of marks) sum += mark; //Example of reading an array and one line to add up the sum of all the grades
-  return sum / marks.length; //"Divide the total marks by the number of grades"
+
 
 console.log ("My letter grade is", calculatedGrade (average), ";because my numerical average is", calculateAverage (marks));
 }
