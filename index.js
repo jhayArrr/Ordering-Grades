33da3d22a5b7d77c74601d48b5118d64ec5c0d67
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-26 13:50:46 -0600

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const marks = [56, 75, 84, 95, 88, 79]; //add grades as an array
+const average1= [56, 75, 84, 95, 88, 79]; //add grades as an array
 
 
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
@@ -7,11 +7,11 @@ console.log ("My letter grade is", calculatedGrade (average1), ";because my nume
 //This function uses another function, call Single Concern Function (able to use either function for various purposes)
 function calculatedGrade (average1) {
   const average2 = calculateAverage (marks);
-  if (calculatedGrade (average2) < 60) return  'F';
-  if (calculatedGrade (average2) < 69) return  'D';
-  if (calculatedGrade (average2) < 79) return  'C';
-  if (calculatedGrade (average2) < 89) return  'B';
-  if (calculatedGrade (average2) <= 100) return 'A'; //This is the last grade left, using the funnel image (metaphor)
+  if (calculatedGrade (average1) < 60) return  'F';
+  if (calculatedGrade (average1) < 69) return  'D';
+  if (calculatedGrade (average1) < 79) return  'C';
+  if (calculatedGrade (average1) < 89) return  'B';
+  if (calculatedGrade (average1) <= 100) return 'A'; //This is the last grade left, using the funnel image (metaphor)
 
   function calculateAverage (arrays) {
     let sum = 0;
