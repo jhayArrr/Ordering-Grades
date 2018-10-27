1c47fd556dfd48a8fb6bbeb425488a39f0688d1a
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-27 00:47:37 -0600

    changes

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const average1 = [99, 90, 75, 89, 78, 84, 45, 69]; //add grades as an array
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
 console.log ("My letter grade is", calculatedGrade (average1), "because my numerical average is", calculateAverage (marks));
 
-//This function uses another function, call Single Concern Function (able to use either function for various purposes)
-function calculatedGrade (average2) {
-  const average1 = calculateAverage (average2);
   if (average2 < 60) return 'F'; //Add appropriate grade or feedback
   if (average2 < 69) return 'D'; //Add approriate conditional
   if (average2 < 79) return 'C';
@@ -15,7 +12,4 @@ function calculatedGrade (average2) {
   return A; //This is the last grade left, using the funnel image (metaphor)
 }
 function calculateAverage (array) {
-  let sum = 0;
-  for (let arrays of average1) sum += marks; //Example of reading an array and one line to add up the sum of all the grades
-  return sum / marks.length; //"Divide the total marks by the number of grades"
 }
