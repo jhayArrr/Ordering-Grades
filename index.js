c0836b837226943792dda6dc0fe7b2c91b1148ed
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-27 00:56:32 -0600

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Variable is an array for security
-const average1 = [99, 90, 75, 89, 78, 84, 45, 69]; //add grades as an array
+const calculatedGrade (average1) = [99, 90, 75, 89, 78, 84, 45, 69]; //add grades as an array
 var marks;
 
 //Use the calculatedGrade function as an output to the console
