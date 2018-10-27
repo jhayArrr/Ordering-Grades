4ac9e5c615e39e7a8b68b4876967e7ceea33861c
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-27 11:23:30 -0600

    changes

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-var score1, score2, score3, score4, score5;
-            Console.WriteLine("Enter Your 5 Grades Below");
-            score1 = GetInput("First", 90);
-            score2 = GetInput("Second",87);
-            score3 = GetInput("Third", 57);
-            score4 = GetInput("Fourth", 56);
-            score5 = GetInput("Last", 84);
-    
-        public static int GetInput(string Order, out int t)
-        {
-            string inValue;
-            Console.WriteLine("Enter the {0} test score: ", Order);
-            inValue = Console.ReadLine();
-            t = Convert.ToInt32(inValue);
-            return t;
-        }
-        public static double CalcAvg (int score1, int score2, int score3, int score4, int score5)
-        {
-            return(score1 + score2 + score3 + score4 + score5) / 5;
-        }
-        public static char DetermineGrade (double avg)
-        {
-            char letterGrade;
-                if (avg > 89)
-                    letterGrade = 'A';
-                else
-                    if (avg > 79)
-                    letterGrade = 'B';
-                    else
-                        if (avg > 69)
-                        letterGrade = 'C';
-                        else
-                            if (avg > 59)
-                            letterGrade = 'D';
-                            else
-                                if (avg <= 59)
-                                letterGrade = 'F';
-        return letterGrade;
-        Console.WriteLine(" Your Grade is a(n)" + letterGrade);
-        Console.ReadKey();
+//Variable is an array for security
+const  average1 = [99, 90, 75, 89, 78, 84, 45, 69]; //add grades as an array
+var average2;
+
+//Use the calculatedGrade function as an output to the console
+//Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
+console.log ("My letter grade is", average1, "because my numerical average is", calculateAverage (average2));
+
+function calculateAverage (average2) {
+  const average1 = calculateAverage (average2);
+  if (average1 < 60) return 'F' ; //Add appropriate grade or feedback
+  if (average1 < 69) return 'D' ; //Add approriate conditional
+  if (average1 < 79) return 'C' ;
+  if (average1 < 89) return 'B' ;
+  if (average1 <= 100) return 'A' ; //This is the last grade left, using the funnel image (metaphor)
 }
+function calculateAverage (array) {
 }
