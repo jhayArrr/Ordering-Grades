7a38394c5f0fc6ad34a75b671bd4c64602045d73
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-27 10:55:04 -0600

    changes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var average2;
 //Use the calculatedGrade function as an output to the console
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
 console.log ("My letter grade is", average1, "because my numerical average is", average2);
-
-function average2 {
+ {
+function average2
   const average1 = average2;
   if (average1 < 60) return 'F'; //Add appropriate grade or feedback
   if (average1 < 69) return 'D'; //Add approriate conditional
