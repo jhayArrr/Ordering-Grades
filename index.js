51f419e11477d2cb0ec1963b987fa55bba984fb0
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-27 10:53:16 -0600

    changes

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var average2;
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
 console.log ("My letter grade is", average1, "because my numerical average is", average2);
 
-function calculatedGrade (average2) {
-  const average1 = calculateAverage (average2);
-  if (average2 < 60) return 'F'; //Add appropriate grade or feedback
-  if (average2 < 69) return 'D'; //Add approriate conditional
-  if (average2 < 79) return 'C';
-  if (average2 < 89) return 'B';
-  if (average2 <= 100) return 'A'; //This is the last grade left, using the funnel image (metaphor)
+function average2 {
+  const average1 = average2;
+  if (average1 < 60) return 'F'; //Add appropriate grade or feedback
+  if (average1 < 69) return 'D'; //Add approriate conditional
+  if (average1 < 79) return 'C';
+  if (average1 < 89) return 'B';
+  if (average1 <= 100) return 'A'; //This is the last grade left, using the funnel image (metaphor)
 }
 function calculateAverage (array) {
 }
