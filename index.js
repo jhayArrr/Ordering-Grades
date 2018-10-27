71ddb7bfe4f9d23c604d15ac7f581c448e3d5616
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-27 00:49:55 -0600

    changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const average1 = [99, 90, 75, 89, 78, 84, 45, 69]; //add grades as an array
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
 console.log ("My letter grade is", calculatedGrade (average1), "because my numerical average is", calculateAverage (marks));
 
+function calculatedGrade (average1) {
+  const average1 = calculateAverage (marks);
   if (average2 < 60) return 'F'; //Add appropriate grade or feedback
   if (average2 < 69) return 'D'; //Add approriate conditional
   if (average2 < 79) return 'C';
