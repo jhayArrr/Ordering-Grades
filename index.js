cee26cecbdb4a34272e03a23055037a8916a3ed0
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-11-05 12:46:02 -0700

    changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //Variable is an array for security
-const  average1 = [89]; //add grades as an array
+const  average1 = [89, 76, 95, 42, 63, 79, 54, 91]; //add grades as an array
 var average2;
 
 //Use the calculatedGrade function as an output to the console
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
-console.log ("My letter grade is", average1, "because my numerical average is", calculateAverage (average2));
+console.log ("My letter grade is", average1, "because my numerical average is", average2);
 
-function calculateAverage (average2) {
+function calculateAverage (average1) {
   const average1 = calculateAverage (average2);
   if (average1 < 60) return 'F' ; //Add appropriate grade or feedback
   if (average1 < 69) return 'D' ; //Add approriate conditional
